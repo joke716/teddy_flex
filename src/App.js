01417d9a7c4c7c1ffd1357e4357e9daf0bb358bb
TeddyKwak <joke79@hanyang.ac.kr>
2022-05-03 10:37:56 +0900

    add movie data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Container, Card, Button, Row, Col} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [movies, setMovies] = useState([])
+
+    const getMovies = async () => {
+        try {
+
+            const result = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US&page=1")
+
+            console.log(result.data.results)
+            setMovies(result.data.results)
+
+        } catch (err) {
+
+        }
+    }
+
+    useEffect(() => {
+        getMovies()
+    }, [])
+
+    // 화면에 보여지는 부분
+    return (
+        <Container className={"mt-3 mb-5"}>
+            <br />
+            <Row>
+                {movies.map(movie => (
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} />
+                            <Card.Body>
+                                <Card.Title>{movie.title}</Card.Title>
+                                <Card.Text>
+                                    {movie.overview.slice(0, 75)} ...
+                                </Card.Text>
+                                <Card.Text>
+                                    평점: {movie.vote_average}
+                                </Card.Text>
+                                <Button variant="secondary">자세히 보기</Button>
+                            </Card.Body>
+                        </Card>
+                        <br />
+                    </Col>
+                ))}
+                <button onClick={() => getMovies()}>
+                    버튼
+                </button>
+            </Row>
+        </Container>
+    );
+};
 
 export default App;
